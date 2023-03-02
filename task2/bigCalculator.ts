aafe966211363826f50570b18c9291e25716467d
DemLoveSky <dem.loginov@gmail.com>
2023-03-02 10:03:16 +0500

    add DivCalculator

--- task2/bigCalculator.ts
+++ task2/bigCalculator.ts
@@ -69,9 +69,19 @@ export function calculateMinus(firstNum: string, secondNum: string): string {
 export function calculateMulti(firstNum: string, secondNum: string): string {
     let digits: Array<number> = [];
     let tempDigit: number = 0;
+    let numberSign: string = "";
 
     if (firstNum === "0" || secondNum === "0") {
         return "0";
+    }
+
+    if (firstNum.startsWith("-") !== secondNum.startsWith("-")) {
+        numberSign = "-";
+        if (firstNum.startsWith("-")) {
+            firstNum = firstNum.slice(1);
+        } else {
+            secondNum = secondNum.slice(1);
+        }
     }
 
     for (let i = 0; i < firstNum.length; i++) {
@@ -107,9 +117,53 @@ export function calculateMulti(firstNum: string, secondNum: string): string {
         tempDigit = 0;
     }
 
-    return digits.reverse().join('');
+    return numberSign + digits.reverse().join('');
+}
+
+function compareStrings(str1: string, str2: string): boolean {
+    if (str1.length > str2.length || str1 === str2) {
+        return true;
+    }
+    return str1.length === str2.length && str1 > str2;
+
 }
 
 export function calculateDiv(firstNum: string, secondNum: string): string {
+    let digits: Array<number> = [];
+    let tempDigit: string = "";
+    let numberSign: string = "";
 
+    if (firstNum === "0" || firstNum.length < secondNum.length) {
+        return "0";
+    }
+    if (secondNum === "0") {
+        throw "Деление на ноль запрещено!";
+    }
+    if (firstNum === secondNum) {
+        return "1";
+    }
+    if (firstNum.startsWith("-") !== secondNum.startsWith("-")) {
+        numberSign = "-";
+    }
+
+    tempDigit = firstNum.slice(0, secondNum.length);
+    for (let i = secondNum.length - 1; i < firstNum.length; i++) {
+        tempDigit += i == secondNum.length - 1 ? "" : firstNum[i];
+        let tempQuotient = 0;
+
+        let multi = calculateMulti(secondNum, String(tempQuotient));
+        do {
+            tempQuotient++;
+            multi = calculateMulti(secondNum, String(tempQuotient));
+        } while (compareStrings(tempDigit, multi));
+        tempQuotient--;
+        if (tempQuotient === -1) {
+            digits.push(0);
+            continue;
+        }
+        digits.push(tempQuotient);
+        tempDigit = String(+calculateMinus(tempDigit, calculateMulti(secondNum, String(tempQuotient))) || "");
+    }
+
+    return numberSign + Number(digits.join('')).toString();
 }
---
+++ task2/main.ts
@@ -0,0 +1,4 @@
+import * as calculator from "./bigCalculator";
+
+const res = calculator.calculateDiv("10000000", "520");
+console.log(res);
